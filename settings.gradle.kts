0d7441227a137c4bb126525f1bab58191f11c8e4
Giung Nam <nam.7loro@gmail.com>
2021-09-29 01:37:21 +0900

    HomeFragment layout 추가 TODO - Service 구현

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -131,3 +131,4 @@ dependencyResolutionManagement {
         }
     }
 }
+include(":shared")
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,9 +23,6 @@ android {
             )
         }
     }
-
-    buildFeatures.viewBinding = true
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,14 +33,12 @@ android {
 }
 
 dependencies {
-    implementation(project(":shared"))
 
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.3.1")
+    implementation("androidx.databinding:viewbinding:4.2.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
